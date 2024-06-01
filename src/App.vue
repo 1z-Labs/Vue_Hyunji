cbde262e91ebe2bf3d181fa2ab359637d6da46f2
Hyunji Kim <s2208@e-mirim.hs.kr>
2024-06-01 21:36:09 +0900

    v-if 활용

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <!--조건부 렌더링 : 디렉티브에 할당된 참/거짓 값에 따라 HTML요소를 렌더링할지 결정하는 것-->
-  <p v-if="visible">이 요소는 렌더링 중</p>
-  <p v-if="unvisible">이 요소는 렌더링 X</p>
+  <p v-if="condition === 'A'">condition의 값은 {{ condition }}</p>
+  <p v-if="condition === 'B'">condition의 값은 {{ condition }}</p>
+  <p v-if="condition === 'C'">condition의 값은 {{ condition }}</p>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      visible: true,
-      unvisible: false, // DOM 추가 후 감춰짐 X / DOM에 추가되지 않음 O
+      condition: 'B'
     };
   }
 }
