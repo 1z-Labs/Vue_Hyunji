48290245a47db4fb5dc4e1033a3c52f362d6282e
Hyunji Kim <s2208@e-mirim.hs.kr>
2024-06-01 21:08:05 +0900

    v-html 전

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,14 @@
 <template>
-  <div>
-    <!--HTML 속성도 데이터 바인딩 가능 -> :(콜론) 사용 (vue 문법)-->
-    <h4 class="red" :style="스타일">{{ products[0] }}</h4>
-    <p>{{ price1 }} 만원</p> <!--{{  }} -> 자동 렌더링-->
-  </div>
-  <div>
-    <h4>{{ products[1] }}</h4>
-    <p>{{ price2 }} 만원</p>
-  </div>
+  <!--이렇게 출력하면 h1 태그까지 문자열로 인식하여 그대로 출력됨-->
+  {{ message }} 
 </template>
 
 <script>
 export default {
-  name: 'App',
-  // 데이터 바인딩 : js 데이터를 html에 넣는 방법
-  // 데이터 보관함! - 자주 변경되는 데이터들은 데이터 바인딩을 이용할 것
-  data(){
-    return{
-      price1 : 60,
-      price2 : 70,
-      스타일 : 'color: red',
-      products : ['역삼동 오피스텔', '마포구 오피스텔'],
-    }
-  },
-  components: {
-   
+  data() {
+    return {
+      message: '<h1>Hello, Vue.JS !</h1>'
+    };
   }
 }
 </script>
